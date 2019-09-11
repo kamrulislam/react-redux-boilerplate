47775d0204101c4cc1c73cbc26656e218e684aaf
Md Kamrul Islam <kamruli@gmail.com>
2019-09-11 17:50:51 +1000

    added redux

---
+++ src/configureStore.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import monitorReducersEnhancer from './enhancers/monitorReducers';
+import loggerMiddleware from './middleware/logger';
+import rootReducer from './reducers/index';
+
+export default function configureStore(preloadedState) {
+  const middlewares = [loggerMiddleware, thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const enhancers = [middlewareEnhancer, monitorReducersEnhancer];
+  // const composedEnhancers = compose(...enhancers);
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+
+  return store;
+};
--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
+// import { createStore } from 'redux';
+// import store from './reducers';
+// import rootReducer from './reducers';
+// import thunkMiddleware from 'redux-thunk';
+// import loggerMiddleware from './middleware/logger';
+// import monitorReducerEnhancer from './enhancers/monitorReducer';
+
+
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware);
+// const composedEnhancers = compose(
+//   middlewareEnhancer,
+//   monitorReducerEnhancer
+// );
+
+// const store = createStore(rootReducer, undefined, composedEnhancers);
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
